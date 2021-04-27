486aa458057e4ae92a5f680435b0438324b71a1e
Eric <ericyeonpark@gmail.com>
2021-04-27 12:44:23 -0700

    update predictions page copy

--- pages/predictions.py
+++ pages/predictions.py
@@ -8,27 +8,45 @@ from dash.dependencies import Input, Output
 # Imports from this application
 from app import app
 
-# 2 column layout. 1st column width = 4/12
-# https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
-column1 = dbc.Col(
+
+row = html.Div(
     [
-        dcc.Markdown(
-            """
-        
-            ## Predictions
-
-            Your instructions: How to use your app to get new predictions.
-
-            """
+        dbc.Row(dbc.Col(html.Div('''Welcome to our predictions page! Fill out the form as accurately as possible,
+                                 and we will compare similar properties (bases on amenities and location) and 
+                                 provide you with the best estimate for your daily rental price on AirBnB.'''))),
+        dbc.Row(
+            [
+                dbc.Col(html.Div("One of three columns")),
+                dbc.Col(html.Div("One of three columns")),
+                dbc.Col(html.Div("One of three columns")),
+            ]
         ),
-    ],
-    md=4,
-)
-
-column2 = dbc.Col(
-    [
-
     ]
 )
 
-layout = dbc.Row([column1, column2])
+layout = row
+
+# # 2 column layout. 1st column width = 4/12
+# # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
+# column1 = dbc.Col(
+#     [
+#         dcc.Markdown(
+#             """
+        
+#             ## Predictions
+
+#             Your instructions: How to use your app to get new predictions.
+
+#             """
+#         ),
+#     ],
+#     md=4,
+# )
+
+# column2 = dbc.Col(
+#     [
+
+#     ]
+# )
+
+# layout = dbc.Row([column1, column2])
