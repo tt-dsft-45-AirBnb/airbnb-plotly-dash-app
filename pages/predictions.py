c2ad04921652e1ad2c48cb78a6a7d41b51129424
Eric <ericyeonpark@gmail.com>
2021-04-27 14:11:51 -0700

    add dropdowns for Property type and Room type

--- pages/predictions.py
+++ pages/predictions.py
@@ -13,12 +13,64 @@ row = html.Div(
     [
         dbc.Row(dbc.Col(html.Div('''Welcome to our predictions page! Fill out the form as accurately as possible,
                                  and we will compare similar properties (bases on amenities and location) and 
-                                 provide you with the best estimate for your daily rental price on AirBnB.'''))),
+                                 provide you with the best estimate for your daily rental price on AirBnB.''', className='mb-4'))),
         dbc.Row(
             [
-                dbc.Col(html.Div("One of three columns")),
-                dbc.Col(html.Div("One of three columns")),
-                dbc.Col(html.Div("One of three columns")),
+                dbc.Col(
+                    [
+                        dcc.Markdown("##### Property Type", className='mb-1'),
+                        dcc.Dropdown(
+                            options=[
+                                {'label': 'Apartment', 'value': 'Apartment'},
+                                {'label': 'House', 'value': 'House'},
+                                {'label': 'Condominium', 'value': 'Condominium'},
+                                {'label': 'Townhouse', 'value': 'Townhouse'},
+                                {'label': 'Loft', 'value': 'Loft'},
+                                {'label': 'Other', 'value': 'Other'},
+                                {'label': 'Guesthouse', 'value': 'Guesthouse'},
+                                {'label': 'Bed & Breakfast', 'value': 'Bed & Breakfast'},
+                                {'label': 'Bungalow', 'value': 'Bungalow'},
+                                {'label': 'Dorm', 'value': 'Dorm'},
+                                {'label': 'Guest suite', 'value': 'Guest suite'},
+                                {'label': 'Villa', 'value': 'Villa'},
+                                {'label': 'Timeshare', 'value': 'Timeshare'},
+                                {'label': 'In-law', 'value': 'In-law'},
+                                {'label': 'Boutique hotel', 'value': 'Boutique hotel'},
+                                {'label': 'Hostel', 'value': 'Hostel'},
+                                {'label': 'Camper/RV', 'value': 'Camper/RV'},
+                                {'label': 'Cabin', 'value': 'Cabin'},
+                                {'label': 'Boat', 'value': 'Boat'},
+                                {'label': 'Serviced apartment', 'value': 'Serviced apartment'},
+                                {'label': 'Castle', 'value': 'Castle'},
+                                {'label': 'Tent', 'value': 'Tent'},
+                                {'label': 'Vacation home', 'value': 'Vacation home'},
+                                {'label': 'Yurt', 'value': 'Yurt'},
+                                {'label': 'Treehouse', 'value': 'Treehouse'},
+                                {'label': 'Chalet', 'value': 'Chalet'},
+                                {'label': 'Hut', 'value': 'Hut'},
+                                {'label': 'Tipi', 'value': 'Tipi'},
+                                {'label': 'Earth House', 'value': 'Earth House'},
+                                {'label': 'Cave', 'value': 'Cave'},
+                                {'label': 'Casa particular', 'value': 'Casa particular'},
+                                {'label': 'Train', 'value': 'Train'}
+                            ],
+                            value='Apartment'
+                        ),  
+                    ],
+                ),
+                dbc.Col(
+                    [
+                        dcc.Markdown("##### Room Type", className='mb-1'),
+                        dcc.Dropdown(
+                            options=[
+                                {'label': 'Entire Home/Apartment', 'value': 'Entire home/apt'},
+                                {'label': 'Private Room', 'value': 'Private room'},
+                                {'label': 'Shared room', 'value': 'Shared room'}
+                            ],
+                            value='Entire home/apt'
+                        ),  
+                    ],
+                ),
             ]
         ),
     ]
