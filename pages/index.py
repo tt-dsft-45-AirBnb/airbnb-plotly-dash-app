233c28e522eea12bdd18e8e4263372c6e768f1ea
Eric <ericyeonpark@gmail.com>
2021-04-27 15:12:12 -0700

    add treehouse image

--- pages/index.py
+++ pages/index.py
@@ -18,11 +18,11 @@ column1 = dbc.Col(
         
             ## Predict your AirBnB Listing Price
 
-            Attention property owners! Are you looking to earn some extra money off the property you own? 
-            Do you feel AirBnB is the most convenient site to help rent out your property? 
-            But you aren't sure what price to exactly list your property?
-            Then you in luck. Our site will help you predict exactly what price you should list your property ny comparing similar
-            properties in your neighborhoor. Click the button below and see how valuable your house is today.
+            ##### Attention property owners! Are you looking to earn some extra money off the property you own? 
+            ##### Do you feel AirBnB is the most convenient site to help rent out your property? 
+            ##### But you aren't sure what price to exactly list your property?
+            ##### Then you in luck. Our site will help you predict exactly what price you should list your property ny comparing similar
+            ##### properties in your neighborhoor. Click the button below and see how valuable your house is today.
             
             """
         ),
@@ -31,13 +31,10 @@ column1 = dbc.Col(
     md=4,
 )
 
-gapminder = px.data.gapminder()
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
-           hover_name="country", log_x=True, size_max=60)
 
 column2 = dbc.Col(
     [
-        dcc.Graph(figure=fig),
+        html.Img(src='assets/treehouse.jpg', width='700px', height='470px'),
     ]
 )
 
